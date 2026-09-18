c088df0dacf2c964d3a56041a30ff7cc210ff82d
Guillaume <guillaumexue@outlook.fr>
2026-09-18 10:49:05 +0200

    ci: exclusion de l'autoconfiguration BDD pour le test contextLoads

--- backend/src/test/java/com/newtrading/api/ApiApplicationTests.java
+++ backend/src/test/java/com/newtrading/api/ApiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.newtrading.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    FlywayAutoConfiguration.class
+})
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Teste que le contexte Spring charge sans exiger une base PostgreSQL active
+    }
 
 }
